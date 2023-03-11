d6f2a8a3aea33f482d4ddc4ccf729ff3b426d642
rafael saraiva <saraiva_cmr@hotmail.com>
2023-03-11 12:14:42 -0300

    create all models classes needed

---
+++ app/src/main/java/br/com/uniritter/app1_2023_1/models/Todo.java
@@ -0,0 +1,48 @@
+package br.com.uniritter.app1_2023_1.models;
+
+public class Todo {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String completed;
+
+    public Todo(int userId, int id, String title, String completed) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+}
---
+++ app/src/main/java/br/com/uniritter/app1_2023_1/models/User.java
@@ -0,0 +1,48 @@
+package br.com.uniritter.app1_2023_1.models;
+
+public class User {
+    private int id;
+    private String name;
+    private String userName;
+    private String email;
+
+    public User(int id, String name, String userName, String email) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
